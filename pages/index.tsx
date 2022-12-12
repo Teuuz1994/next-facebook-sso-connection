ee2efd95ff6742b4844664af6d804125a3e701d9
Matheus da Guia de Campos <matheus.rj.ig@gmail.com>
2022-12-12 20:07:12 -0300

    navigatio login loggic

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,22 @@
 import { signIn } from 'next-auth/react';
+import { useRouter } from 'next/router';
 
 export default function Home() {
+  const router = useRouter()
+
+  const tryToLoginWithFacebook = async () => {
+    try {
+      await signIn('facebook')
+      router.push('/logged')
+    } catch (error) {
+      console.log('Erro ao efetuar login')
+      console.error(error)
+    }
+  }
+
   return (
     <div>
-      <button type="button" onClick={() => signIn('facebook')}>login com o Facebook</button>
+      <button type="button" onClick={tryToLoginWithFacebook}>login com o Facebook</button>
     </div>
   )
 }
