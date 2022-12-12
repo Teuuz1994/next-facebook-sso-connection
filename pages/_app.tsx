affa44a0ba78c62eda83f1677a7c814b020426d3
Matheus da Guia de Campos <matheus.rj.ig@gmail.com>
2022-12-12 19:44:21 -0300

    fix: session

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,15 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import { SessionProvider } from 'next-auth/react';
+import { SessionProvider, } from 'next-auth/react';
+import { Session } from 'next-auth';
 
-export default function App({ Component, pageProps }: AppProps) {
+interface MyAppProps extends AppProps {
+  session: Session | null | undefined
+}
+
+export default function App({ Component, session, pageProps }: MyAppProps) {
   return (
-    <SessionProvider>
+    <SessionProvider session={session}>
       <Component {...pageProps} />
     </SessionProvider>
   )
