0e971f3a20753279695f63f4cb7a5a91818216de
Matheus da Guia de Campos <matheus.rj.ig@gmail.com>
2022-12-12 23:36:15 -0300

    change log out method

--- pages/logged.tsx
+++ pages/logged.tsx
@@ -1,7 +1,19 @@
 import { useSession, signOut } from 'next-auth/react'
+import { useRouter } from 'next/router'
 
 export default function Logged() {
   const { data, status } = useSession()
+  const router = useRouter()
+
+  const logOut = async () => {
+    try {
+      await signOut()
+      router.push('/')
+    } catch (error) {
+      console.log('não foi possível deslogar usuário')
+      console.error(error)
+    }
+  }
 
   return (
     <div>
@@ -10,7 +22,7 @@ export default function Logged() {
           <h1>logged in</h1>
           <span>name: {data?.user?.name}</span>
           <span>email: {data?.user?.email}</span>
-          <button type="button" onClick={() => signOut()}>Log out</button>
+          <button type="button" onClick={logOut}>Log out</button>
         </>
       )}
       {status === 'unauthenticated' && (<h1>usuário não logado</h1>)}
