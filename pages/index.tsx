3011d28279b3122cf659568d100f64ec99877ea5
Matheus da Guia de Campos <matheus.rj.ig@gmail.com>
2022-12-12 23:19:12 -0300

    change push navigation

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,26 @@
 import { signIn } from 'next-auth/react';
 import { useRouter } from 'next/router';
+import { useSession } from 'next-auth/react';
+import { useEffect } from 'react';
 
 export default function Home() {
   const router = useRouter()
+  const { status } = useSession()
 
   const tryToLoginWithFacebook = async () => {
     try {
       await signIn('facebook')
-      router.push('/logged')
     } catch (error) {
       console.log('Erro ao efetuar login')
       console.error(error)
     }
   }
+
+  useEffect(() => {
+    if (status === 'authenticated') {
+      router.push('/logged')
+    }
+  }, [router, status])
 
   return (
     <div>
